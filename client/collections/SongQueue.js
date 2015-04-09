c67fd3a58b02c8f9fb853dfd00805f37e594fd78
stevenshuhyow <stevenshuhyow@gmail.com>
2015-04-08 17:35:12 -0700

    Completed test cases for SongModel, working on SongQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,24 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
+  initialize: function(params){
+    this.on('add', function(){
+        if (this.length === 1){
+          this.playFirst();
+        }
+    }, this);
+
+    this.on('ended', function(){
+      console.log(this);
+      this.shift();
+    },this);
+
+  },
+
+  playFirst: function(){
   }
+
+
+
 
 });
